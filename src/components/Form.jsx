bceff467cd5e471284377fcf20fa284c1cf14645
drey <dreybrizuela12@gmail.com>
2025-01-31 09:59:24 +0800

    fix

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import './cssform.css';
+import './css/form.css';
 export const ContactUs = () => {
   const form = useRef();
 
